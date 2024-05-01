d955bda4121c9df61f4b296f5bf3cfd80a3ec7d3
Wanderleyps <wanderleyps794@gmail.com>
2024-05-01 17:38:49 -0300

    add repository

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import type TipoPet from "../tipos/TipoPet";
 import EnumEspecie from "../enum/enumEspecie";
+import PetRepository from "../repositories/PetRepository";
+import PetEntity from "../entities/PetEntity";
 
 let listaDePets: Array<TipoPet> = [];
 let id = 0;
@@ -10,51 +12,56 @@ function geraId() {
   return id;
 }
 
-
 export default class PetController {
+  constructor(private repository: PetRepository) {}
 
-    listaPets(req: Request, res: Response){
-        return res.status(200).json(listaDePets);
-    }
-    
-    criaPet(req: Request, res: Response){
-        const { nome, especie, adotado, dataNascimento} = <TipoPet>req.body;
-        const novoPet: TipoPet = {id: geraId(), nome, especie, adotado, dataNascimento};
-        if (!Object.values(EnumEspecie).includes(especie)) {
-            return res.status(404).json({ erro: "Espécie inválida" })
-        }
-        listaDePets.push(novoPet);
-        return res.status(201).json(novoPet);
+  listaPets(req: Request, res: Response) {
+    return res.status(200).json(listaDePets);
+  }
+
+  criaPet(req: Request, res: Response) {
+    const { adotado, especie, dataNascimento, nome } = <PetEntity>req.body;
+
+    if (!Object.values(EnumEspecie).includes(especie)) {
+      return res.status(400).json({ error: "Especie inválida" });
     }
 
-    atualizarPet(req: Request, res: Response){
-        const { id } = req.params;
-        const { nome, especie, adotado, dataNascimento} = <TipoPet>req.body;
-        const pet = listaDePets.find((pet) => pet.id === Number(id));
-        if (!pet) {
-            return res.status(404).json({ erro: "Pet não encontrado" });
-        }
-        if (!Object.values(EnumEspecie).includes(especie)) {
-            return res.status(404).json({ erro: "Espécie inválida" })
-        }
-        pet.nome = nome;
-        pet.dataNascimento = dataNascimento;
-        pet.especie = especie;
-        pet.adotado = adotado;
-        return res.status(200).json(pet);
+    const novoPet = new PetEntity();
+    novoPet.id = geraId();
+    novoPet.adotado = adotado;
+    novoPet.especie = especie;
+    novoPet.dataNascimento = dataNascimento;
+    novoPet.nome = nome;
+    this.repository.criaPet(novoPet);
+    return res.status(201).json(novoPet);
+  }
+
+  atualizarPet(req: Request, res: Response) {
+    const { id } = req.params;
+    const { nome, especie, adotado, dataNascimento } = <TipoPet>req.body;
+    const pet = listaDePets.find((pet) => pet.id === Number(id));
+    if (!pet) {
+      return res.status(404).json({ erro: "Pet não encontrado" });
     }
-
-    deletarPet(req: Request, res: Response){
-        //const { id } = <TipoPet>req.body;
-        const { id } = req.params;
-        const pet = listaDePets.find((pet) => pet.id === Number(id));
-        if (!pet) {
-            return res.status(404).json({ erro: "Pet não encontrado" });
-        }
-        const index = listaDePets.indexOf(pet);
-        listaDePets.splice(index, 1);
-        return res.status(200).json({ mensagem: "Pet deletado com sucesso"});
+    if (!Object.values(EnumEspecie).includes(especie)) {
+      return res.status(404).json({ erro: "Espécie inválida" });
     }
+    pet.nome = nome;
+    pet.dataNascimento = dataNascimento;
+    pet.especie = especie;
+    pet.adotado = adotado;
+    return res.status(200).json(pet);
+  }
 
-
+  deletarPet(req: Request, res: Response) {
+    //const { id } = <TipoPet>req.body;
+    const { id } = req.params;
+    const pet = listaDePets.find((pet) => pet.id === Number(id));
+    if (!pet) {
+      return res.status(404).json({ erro: "Pet não encontrado" });
+    }
+    const index = listaDePets.indexOf(pet);
+    listaDePets.splice(index, 1);
+    return res.status(200).json({ mensagem: "Pet deletado com sucesso" });
+  }
 }
--- src/routes/petRouter.ts
+++ src/routes/petRouter.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import PetController from "../controller/PetController"; // Adjusted import path
+import PetRepository from "../repositories/PetRepository";
+import { AppDataSource } from "../config/dataSource";
 
 const router = express.Router();
+const petRepository = new PetRepository(AppDataSource.getRepository("PetEntity"));
+const petController = new PetController(petRepository); // Instantiate PetController here
 
-const petController = new PetController(); // Instantiate PetController here
-
-router.get("/", petController.listaPets);
-router.post("/", petController.criaPet);
-router.put("/:id", petController.atualizarPet);
-router.delete("/:id", petController.deletarPet);
+// maneira alternativa
+// router.post("/", petController.criaPet.bind(petController));
+router.post("/", (req, res) => petController.criaPet(req, res));
+router.get("/", (req, res) => petController.listaPets(req, res));
+router.put("/:id", (req, res) => petController.atualizarPet(req, res));
+router.delete("/:id", (req, res) => petController.deletarPet(req, res));
 
 export default router;
